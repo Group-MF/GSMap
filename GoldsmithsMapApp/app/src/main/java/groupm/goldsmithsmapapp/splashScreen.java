698c3fba60f7aa0d96d92ab29e23854a4a031feb
Joel Mayner <jmayn001@gold.ac.uk>
2017-05-03 15:01:59 +0100

    random-update-2

--- GoldsmithsMapApp/app/src/main/java/groupm/goldsmithsmapapp/splashScreen.java
+++ GoldsmithsMapApp/app/src/main/java/groupm/goldsmithsmapapp/splashScreen.java
@@ -1,13 +1,28 @@
 package groupm.goldsmithsmapapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class splashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
     }
+
+    /*
+    "Touch to Enter"
+    Invisible button (or interactive image) is used to trigger the menu activity
+
+     */
+    public void enterMenu(View view){
+        Intent toMenu = new Intent(this, MainMenu.class);
+
+
+
+    }
+
 }
