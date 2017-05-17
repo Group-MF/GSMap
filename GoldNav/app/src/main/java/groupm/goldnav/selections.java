96ff7dbcff543727659b60c7470c5fadb9e223bb
msamu001 <msamu001@gold.ac.uk>
2017-05-17 21:45:42 +0100

    basic java screen 2

---
+++ GoldNav/app/src/main/java/groupm/goldnav/selections.java
@@ -0,0 +1,13 @@
+package com.groupm.goldNav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selections);
+    }
+}
