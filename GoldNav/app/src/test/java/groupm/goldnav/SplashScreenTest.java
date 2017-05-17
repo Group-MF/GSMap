50be1dbfa16036f77b91e2906a8347eeaf1192eb
Joel Mayner <jmayn001@gold.ac.uk>
2017-05-17 21:47:01 +0100

    FUCKING WORKS!!!!!!!!!!!*
    *displays on the emulator

---
+++ GoldNav/app/src/test/java/groupm/goldnav/SplashScreenTest.java
@@ -0,0 +1,14 @@
+package groupm.goldnav;
+
+import org.junit.Test;
+
+/**
+ * Created by Joel on 17/05/2017.
+ */
+public class SplashScreenTest {
+	@Test
+	public void onCreate() throws Exception {
+
+	}
+
+}
