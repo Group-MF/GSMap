11ffe4430d539f71b06ea3a0820593dc46bfa773
msamu001 <msamu001@gold.ac.uk>
2017-05-19 20:25:01 +0100

    Working Version - 2 pages

---
+++ GoldNav/app/src/main/main/java/groupm/goldnav/SplashScreen.java
@@ -0,0 +1,27 @@
+package groupm.goldnav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.constraint.ConstraintLayout;
+import android.view.View;
+import com.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+
+		// Adds click listener to whole screen
+		ConstraintLayout splashScreen = (ConstraintLayout)findViewById(R.id.screen1);
+		splashScreen.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent nextPage = new Intent(v.getContext(), Selections.class);
+				startActivity(nextPage);
+				finish();
+			}
+		});
+	}
+}
