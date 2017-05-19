31c1fb9a9d8473c25a6fc9f86d58ea9995ac9219
Joel Mayner <jmayn001@gold.ac.uk>
2017-05-19 18:46:57 +0100

    Some buttons
    Avoid go for now

---
+++ GoldNav/app/src/main/java/groupm/goldnav/Search.java
@@ -0,0 +1,15 @@
+package groupm.goldnav;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.R;
+
+public class Search extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_search);
+	}
+}
