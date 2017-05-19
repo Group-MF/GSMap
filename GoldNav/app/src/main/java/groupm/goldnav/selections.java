4787b7b523559e4483bf7dec46f054095b3e7638
msamu001 <msamu001@gold.ac.uk>
2017-05-19 15:25:53 +0100

    Working Version

--- GoldNav/app/src/main/java/groupm/goldnav/selections.java
+++ GoldNav/app/src/main/java/groupm/goldnav/selections.java
@@ -1,13 +1,12 @@
-package com.groupm.goldNav;
+package groupm.goldnav;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class selections extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selections);
+        setContentView(com.R.layout.activity_selections);
     }
 }
