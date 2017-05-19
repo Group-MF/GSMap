06199ae9577a93e33d9d4806fbee0e502328f835
Joel Mayner <jmayn001@gold.ac.uk>
2017-05-19 19:21:45 +0100

    ButtonFixes

--- GoldNav/app/src/main/java/groupm/goldnav/Search.java
+++ GoldNav/app/src/main/java/groupm/goldnav/Search.java
@@ -10,6 +10,6 @@ public class Search extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_search);
+		setContentView(R.layout.activity_search);
 	}
 }
