8631a04649bc804922b2f0e871193bd09a11f68c
msamu001 <msamu001@gold.ac.uk>
2017-05-20 21:55:50 +0100

    Updated Functionality

--- GoldNav/app/src/main/java/groupm/goldnav/FindRoom.java
+++ GoldNav/app/src/main/java/groupm/goldnav/FindRoom.java
@@ -4,21 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
 
 import com.R;
 
+import java.io.IOException;
+
 import Pathfinder.Options;
+import Pathfinder.Searcher;
 
 public class FindRoom extends AppCompatActivity {
 
     private Options toggle;
+    private Searcher rooms;
+    public AutoCompleteTextView destinationACTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_room);
+        /*
+        destinationACTV = (AutoCompleteTextView)findViewById(R.id.enterDestination);
         toggle = new Options();
+        try{
+            rooms = new Searcher();
+        } catch(IOException e) {
+            System.err.print(e.toString());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, rooms.getRooms());
+        destinationACTV.setAdapter(adapter);
+        */
     }
 
     public void toggleAccess(View view) {
