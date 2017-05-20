2639ae8a9b3e4ff6d6ff9061496fea8f90ff79ca
Joel Mayner <jmayn001@gold.ac.uk>
2017-05-20 19:03:42 +0100

    uhhhh

--- GoldNav/app/src/main/java/groupm/goldnav/ShowMap.java
+++ GoldNav/app/src/main/java/groupm/goldnav/ShowMap.java
@@ -1,46 +1,45 @@
 package groupm.goldnav;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
+
 import com.R;
 
 public class ShowMap extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_map);
-    }
-    /*
-    //onClick up button
-    public void floorUp() {
-        ImageView map = (ImageView)findViewById(R.id.RHB_Map);
-        map.setImageResource(R.drawable.rhb_f2);
-        /*
-        if(map.getDrawable()) { // If 1st floor is displayed
-            map.setImageResource(R.drawable.rhb_f2);      // Display 2nd floor
-        } else if(map.getDrawable()) { // If 2nd floor is displayed
-            map.setImageResource(R.drawable.rhb_f3);             // Display 3rd floor
-        }
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_map);
+	}
+}
+/*
+		// Creates button variables
+        Button floorUp = (Button)findViewById(R.id.showRHB);
+        Button floorDown = (Button)findViewById(R.id.showRHB);
 
-    // onClick down button
-    public void floorDown() {
-        ImageView map = (ImageView) findViewById(R.id.RHB_Map);
+        // Adds click listener to up button
+        floorUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ImageView map = (ImageView)findViewById(R.id.showMapB);
+                if(map.getDrawable().equals(R.drawable.rhb_f1)) { // If 1st floor is displayed
+                    map.setImageResource(R.drawable.rhb_f2);      // Display 2nd floor
+                } else if(map.getDrawable().equals(R.drawable.rhb_f2)) { // If 2nd floor is displayed
+                    map.setImageResource(R.drawable.rhb_f3);             // Display 3rd floor
+                }
+            }
+        });
 
-        /*
-        if (map.getDrawable().equals(R.drawable.rhb_f2)) {
-            map.setImageResource(R.drawable.rhb_f1);
-        } else if (map.getDrawable().equals(R.drawable.rhb_f3)) {
-            map.setImageResource(R.drawable.rhb_f2);
-        }
-
-    }
-    */
-    public void onBackPressed() {
-        Intent i = new Intent(this, Selections.class);
-        startActivity(i);
+        // Adds click listener to down button
+        floorDown.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ImageView map = (ImageView)findViewById(R.id.RHB_Map);
+                if(map.getDrawable().equals(R.drawable.rhb_f2)) {
+                    map.setImageResource(R.drawable.rhb_f1);
+                } else if(map.getDrawable().equals(R.drawable.rhb_f3)) {
+                    map.setImageResource(R.drawable.rhb_f2);
+                }
+            }
+        });
     }
 }
+*/
